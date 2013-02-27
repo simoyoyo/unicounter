f1418da74ee7557f1830f4dc22531c94970c6357
bravo <bravo@LAB-INFO-LIM.INFO.ITISCORNI.IT>
2013-02-27 09:56:08 +0100

    Prima release

---
+++ src/contatoreuniversale/ContatoreUniversale.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contatoreuniversale;
+
+/**
+ *
+ * @author Simoyoyo
+ * palla
+ */
+public class ContatoreUniversale {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        // TODO code application logic here
+    }
+}
