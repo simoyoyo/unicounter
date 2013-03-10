7d2f7d37b9c27c03fde5d7006e7686d6799ed408
piffy <Prof. Missiroli>
2013-03-10 14:12:58 +0100

    - Aggiunta main di test - Preparazione per lo sviluppo parallelo.

--- src/contatoreuniversale/ContatoreUniversale.java
+++ src/contatoreuniversale/ContatoreUniversale.java
@@ -1,25 +1,56 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package contatoreuniversale;
 
 /**
- *
+ *Questa classe dovrebbe fornire tutte le funzionalità per 
+ * il funzionamento di un contatore universale da usare in tutti i possibili
+ * cicli.
+ * 
  * @author Simoyoyo
- * @authot Piffy
+ * @author Piffy
  */
+
+//TODO Implementare Comparable
+
 public class ContatoreUniversale {
-    
+
     int x;
 
+    //TODO Contatore con Inizializzatore
+    
+    /**
+     * Costruttore standard: inizializza a zero
+     */
     public ContatoreUniversale() {
         x=0;
     }
-
+    
+    //TODO Incrementa() 
+    
+    //TODO Incrementa(int incremento) 
+    
+    //TODO ResetToInitialvalue (req. new attribute)
+    
+    /**
+     * 
+     * @return il valore di x convertito in Stringa 
+     */
     @Override
     public String toString() {
         return ""+x;
     }
     
+    public static void main(String args[]) {
+    //Test of counter
+        ContatoreUniversale c = new ContatoreUniversale();
+        System.out.println("Valore (dovrebbe essere 0):"+c);
+    //Test of Incrementa
+
+    //Test of Incrementa con quantità di incremento    
+        
+    //Test of Contatore con Inizializzatore
+        
+    //Test of Comparable interface
+        
+    //Test of ResetToInitialvalue()    
+    }
 }
